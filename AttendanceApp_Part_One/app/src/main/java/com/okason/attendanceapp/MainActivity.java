a0c1b9037ec1b207beb5ab67313ebc36349cc62c
Val Okafor <okaatech@yahoo.com>
2015-06-14 08:44:39 -0700

    Updated Toolbar

--- AttendanceApp_Part_One/app/src/main/java/com/okason/attendanceapp/MainActivity.java
+++ AttendanceApp_Part_One/app/src/main/java/com/okason/attendanceapp/MainActivity.java
@@ -1,17 +1,21 @@
 package com.okason.attendanceapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
