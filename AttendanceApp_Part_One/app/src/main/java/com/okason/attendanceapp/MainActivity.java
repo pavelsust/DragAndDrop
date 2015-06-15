78089673600c4d9da6ef4bc4e03b6b5102198931
Val Okafor <okaatech@yahoo.com>
2015-06-14 22:34:21 -0700

    Add Navigation Drawer

--- AttendanceApp_Part_One/app/src/main/java/com/okason/attendanceapp/MainActivity.java
+++ AttendanceApp_Part_One/app/src/main/java/com/okason/attendanceapp/MainActivity.java
@@ -1,21 +1,65 @@
 package com.okason.attendanceapp;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.okason.attendanceapp.adapters.NavDrawerAdapter;
+import com.okason.attendanceapp.models.DrawerItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
 
+    public String HEADER_NAME = "Val Okafor";
+    public String HEADER_EMAIL = "valokafor@someemail.com";
+    public int HEADER_IMAGE = R.drawable.val_okafor;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private DrawerLayout Drawer;
+    private RecyclerView.Adapter mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private List<DrawerItem> dataList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        dataList = new ArrayList<DrawerItem>();
+        addItemsToDataList();
+        mAdapter = new NavDrawerAdapter(dataList, this, HEADER_NAME, HEADER_EMAIL, HEADER_IMAGE);
+        mRecyclerView.setAdapter(mAdapter);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, Drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        Drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState(); //onTouchDrawer(currentFragment);
     }
 
     @Override
@@ -39,4 +83,9 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void addItemsToDataList() {
+        dataList.add(new DrawerItem(getString(R.string.title_attendants), R.drawable.ic_action_attendants));
+        dataList.add(new DrawerItem(getString(R.string.title_events), R.drawable.ic_action_events));
+        dataList.add(new DrawerItem(getString(R.string.title_registration), R.drawable.ic_action_attendant));}
 }
