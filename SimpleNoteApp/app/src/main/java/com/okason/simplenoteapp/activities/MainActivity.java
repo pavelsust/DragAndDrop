39f945700cd5f2b53f6b2bca066dceae762d1358
Val Okafor <okaatech@yahoo.com>
2015-10-18 06:13:30 -0700

    Add new project Drag and Drop

--- SimpleNoteApp/app/src/main/java/com/okason/simplenoteapp/activities/MainActivity.java
+++ SimpleNoteApp/app/src/main/java/com/okason/simplenoteapp/activities/MainActivity.java
@@ -1,14 +1,29 @@
 package com.okason.simplenoteapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 import com.okason.simplenoteapp.R;
-import com.okason.simplenoteapp.data.DatabaseHelper;
+import com.okason.simplenoteapp.fragments.NoteLinedEditorFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    private com.mikepenz.materialdrawer.Drawer result = null;
 
 
     @Override
@@ -16,14 +31,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        databaseHelper.getWritableDatabase();
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        //Now build the navigation drawer and pass the AccountHeader
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.title_home).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.title_settings).withIcon(FontAwesome.Icon.faw_list).withIdentifier(2)
+
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int i, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable){
+                            String name = ((Nameable)drawerItem).getName().getText(MainActivity.this);
+                            mToolbar.setTitle(name);
+                        }
+
+                        if (drawerItem != null){
+                            int selectedScren = drawerItem.getIdentifier();
+                            switch (selectedScren){
+                                case 1:
+                                    //do nothing
+                                    break;
+                                case 2:
+                                    //go to settings screen, yet to be added
+                                    //this will be your home work
+                                    Toast.makeText(MainActivity.this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                            }
+                        }
+                        return false;
+                    }
+
+
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View view) {
+                        KeyboardUtil.hideKeyboard(MainActivity.this);
+
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View view) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View view, float v) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+
+        if (savedInstanceState == null){
+            NoteLinedEditorFragment fragment = new NoteLinedEditorFragment();
+            openFragment(fragment, "Note Editor");
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -40,5 +119,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openFragment(final Fragment fragment, String title){
+           getSupportFragmentManager()
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+        getSupportActionBar().setTitle(title);
     }
 }
